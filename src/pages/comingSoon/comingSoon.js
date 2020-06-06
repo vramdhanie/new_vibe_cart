10d06b402fde5b76c561eda4cdfd0dd3d33cc358
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-06-06 15:02:53 -0400

    Swapped out the main logo

--- src/pages/comingSoon/comingSoon.js
+++ src/pages/comingSoon/comingSoon.js
@@ -3,9 +3,8 @@ import logo from "../../images/logo.png";
 
 const ComingSoon = () => {
   return (
-    <div className="bg-teal-800 w-full h-full flex justify-center items-center">
+    <div className="bg-gray-800 w-full h-full flex justify-center items-center">
       <div className="w-1/2 text-center">
-        <div className="text-6xl text-white">New Vibe!</div>
         <div className="text-center my-4">
           <img src={logo} alt="logo" className="w-1/2 my-0 mx-auto" />
         </div>
