e850d402bdb2ea311ec821dcb2f5ea306b6bf9ef
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-06-22 17:26:11 -0400

    Add placeholder search form

---
+++ src/components/search/searchForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  FormControl,
+  Input,
+  Flex,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/core";
+
+const SearchForm = () => {
+  return (
+    <Flex flex={1} alignItems="center" justifyContent="center">
+      <form className="flex-1">
+        <FormControl width="full">
+          <InputGroup>
+            <Input width="full" />
+            <InputRightElement width="4.5rem">
+              <Button h="100%" size="sm">
+                Search
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+      </form>
+    </Flex>
+  );
+};
+
+export default SearchForm;
