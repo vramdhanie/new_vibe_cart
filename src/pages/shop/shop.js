082bb0ebb5cc7d8b45d71826257e8253231eb161
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-06-22 17:28:26 -0400

    Remove extraneous lines from the main shop component

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -11,6 +11,7 @@ const Shop = () => {
   return (
     <div className="">
       <CartSummary />
+
       <div className="w-11/12 p-2 mx-auto">
         <h2 className="font-bold text-xl text-blue-800 px-4 py-2">Specials</h2>
         <div className="my-2 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 row-gap-3 col-gap-3">
